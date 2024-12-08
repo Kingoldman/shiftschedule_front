f422654cd6ce74e31f03f4acdd97bcd0591a41c1
wanghaiyang <479260115@qq.com>
2024-12-08 21:58:31 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -22,4 +22,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  // 推送githubpages dist
+  base: './',
+  build: {
+    outDir: 'docs',
+  },
 });
